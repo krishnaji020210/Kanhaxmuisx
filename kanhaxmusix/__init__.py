056596fdef42ba8f336e9cf21667a236f0fb1aea
krishnaji020210 <krishnaji020210@gmail.com>
2026-04-05 15:49:06 +0530

    Create __init__.py

---
+++ kanhaxmusix/__init__.py
@@ -0,0 +1,41 @@
+# -----------------------------------------------
+# 🔸 ShashankMusic Project
+# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
+# 📅 Copyright © 2025 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by ItzShukla
+# -----------------------------------------------
+
+from kanhaxmusix.core.bot import Shashank
+from kanhaxmusix.core.dir import dirr
+# from kanhaxmusix.core.git import git
+from kanhaxmusix.core.userbot import Userbot
+from kanhaxmusix.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+# git()
+dbb()
+heroku()
+
+app = kanha()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
