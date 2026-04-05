7bc071374f54b39008667b61c6bd43dae55301c7
krishnaji020210 <krishnaji020210@gmail.com>
2026-04-05 17:06:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# ---------------------------------------------------------------
-# 🔸 ShashankMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
+# -----------------------------------------------
+# 🔸 kanhaxmuisx Project
+# 🔹 Developed & Maintained by: KRISHNA (https://github.com/krishnaji020210)
 # 📅 Copyright © 2025 – All Rights Reserved
 #
 # 📖 License:
@@ -9,8 +9,8 @@
 # Commercial use, redistribution, or removal of this notice is strictly prohibited
 # without prior written permission from the author.
 #
-# ❤️ Made with dedication and love by ItzShukla
-# ---------------------------------------------------------------
+# ❤️ Made with dedication and love by KRISHNA
+# -----------------------------------------------
 
 import re
 from os import getenv
